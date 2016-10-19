eb34550128c0cfaf9c360a25c7a1775fe42b2aa2
Ivan ROGER <Ivan ROGER>
2016-10-19 14:59:36 +0100

    List of ships in BoardTile

--- src/core/BoardBlackholeTile.java
+++ src/core/BoardBlackholeTile.java
@@ -1,9 +1,17 @@
 package core;
 
+import core.exception.UnallowedEx;
+
 public class BoardBlackholeTile extends BoardTile {
 
 	public BoardBlackholeTile(GameBoard myBoard, int xPos, int yPos) {
 		super(myBoard, xPos, yPos);
+	}
+	
+	@Override
+	public void addShip(Ship ship) throws UnallowedEx {
+		if (!EnemyShip.class.isAssignableFrom(ship.getClass())) throw new UnallowedEx("MotherShip can't go on this Tile");
+		
 	}
 
 }
